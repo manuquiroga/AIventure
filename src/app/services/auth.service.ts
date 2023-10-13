ff7d88081b3eccc4fe9de7af45d9fd233024d197
egarciairizar <ezegarciai@hotmail.com>
2023-10-13 18:19:49 -0300

    Connected Firebase with GoogleAuth and the app

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithPopup, signOut, GoogleAuthProvider } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: Auth) { }
+
+  loginWithGoogle(){
+    return signInWithPopup(this.auth, new GoogleAuthProvider());
+  }
+
+  logout(){
+    return signOut(this.auth);
+  }
+}
+  
