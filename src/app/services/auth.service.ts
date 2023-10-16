4a6cd002f3acbb83d6d9591f05ef80201675699b
Gabriel <128416767+GonzalezCrisanto@users.noreply.github.com>
2023-10-16 20:25:44 -0300

    Modify user interface and auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -40,23 +40,42 @@ export class AuthService {
     return this.updateUserData(credential.user);
   }
 
-  private updateUserData(user) {
+  private async updateUserData(user) {
     // Sets user data to firestore on login
     const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+  
+    const snapshot = await userRef.get().toPromise();
+    if (snapshot && snapshot.exists) {
+      const data = snapshot?.data();
 
-    const data = { 
-      uid: user.uid, 
+    const updatedData: Partial<User> = { 
+      uid: user.uid ?? '', 
       email: user.email, 
       displayName: user.displayName, 
       photoURL: user.photoURL,
-      character1: user.character1 || null,
-      character2: user.character2 || null,
-      character3: user.character3 || null,
-      historias: user.historias || 0
+      character1: data && data.character1 !== null ? data.character1 : null,
+      character2: data && data.character2 !== null ? data.character2 : null,
+      character3: data && data.character3 !== null ? data.character3 : null,
+      historias: data && data.historias !== null ? data.historias : 0
     } 
-    
-    return userRef.set(data, { merge: true })
+      
+      return userRef.set(updatedData as User, { merge: true });
+    } else {
+      const initialData: Partial<User> = {
+        uid: user.uid ?? '', 
+        email: user.email, 
+        displayName: user.displayName, 
+        photoURL: user.photoURL,
+        character1: null,
+        character2: null,
+        character3: null,
+        historias: 0
+      }
+  
+      return userRef.set(initialData as User);
+    }
   }
+  
 
   async signOut() {
     await this.firebaseAuthenticationService.signOut();
