e3f4d52db435e41863fbe8b9d48760a00780af0c
egarciairizar <ezegarciai@hotmail.com>
2023-10-18 23:04:51 -0300

    Added charactercreation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CharacterMenuComponent } from './pages/character-menu/character-menu.component';
+import { CharacterCreationComponent } from './components/character-creation/character-creation.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'characters', component: CharacterMenuComponent, canActivate: [AuthGuard] },
-  { path: 'charactercreation', component: CharacterMenuComponent },
+  { path: 'charactercreation', component: CharacterCreationComponent, canActivate: [AuthGuard] },
 
 ];
 
