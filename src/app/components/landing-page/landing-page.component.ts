9595bb0f067104b170b6c2a2b3ce5dde2c107ebe
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-10-19 16:38:34 -0300

    loading animation

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 
 @Component({
@@ -7,6 +8,16 @@ import { Router } from '@angular/router';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
-  constructor(private router: Router) {}
+export class LandingPageComponent implements OnInit{
+  constructor(private router: Router, private auth: AuthService) {}
+
+  showLoadedTemplate: boolean = false;
+
+  ngOnInit() {
+    if(this.auth.user$){
+      setTimeout(() => {
+        this.showLoadedTemplate = true;
+      }, 2500);
+    }
+  }
 }
