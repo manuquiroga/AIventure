57e97dae5462142776ebd36dfc5af4fb58425ec9
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-10-19 19:09:55 -0300

    clean animation

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -8,16 +8,12 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent implements OnInit{
+export class LandingPageComponent{
+  isLoading = false;
   constructor(private router: Router, private auth: AuthService) {}
 
-  showLoadedTemplate: boolean = false;
-
-  ngOnInit() {
-    if(this.auth.user$){
-      setTimeout(() => {
-        this.showLoadedTemplate = true;
-      }, 2500);
-    }
+  onAnimationComplete() {
+    this.isLoading = true;
   }
+
 }
