17573202e4883b70421e272cdc21b4fda23ca25f
egarciairizar <ezegarciai@hotmail.com>
2023-10-21 14:40:06 -0300

    no me acuerdo que agregue

---
+++ src/app/components/attribute-distribution/attribute-distribution.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {CdkMenuItemRadio,
+  CdkMenuItemCheckbox,
+  CdkMenuGroup,
+  CdkMenu,
+  CdkMenuTrigger,
+  CdkMenuItem,
+  CdkMenuBar,} from '@angular/cdk/menu';
+
+@Component({
+  selector: 'app-attribute-distribution',
+  templateUrl: './attribute-distribution.component.html',
+  styleUrls: ['./attribute-distribution.component.css']
+})
+export class AttributeDistributionComponent {
+
+}
