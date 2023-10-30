b6aa84e866d51d5e2cdaa2aae9aa06dc0e2b7050
Gabriel <128416767+GonzalezCrisanto@users.noreply.github.com>
2023-10-30 15:06:51 -0300

    We fixed errors in the AuthService that generated errors in other components

--- src/app/components/non-character-card/non-character-card.component.ts
+++ src/app/components/non-character-card/non-character-card.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,OnDestroy } from '@angular/core';
 import { Character } from 'src/app/models/character.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { Subscription } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-non-character-card',
   templateUrl: './non-character-card.component.html',
   styleUrls: ['./non-character-card.component.css']
 })
-export class NonCharacterCardComponent {
-  characters: Character[] = [];
-  maxCharacterCount: number = 3; // Establece el límite de personajes
+
+export class NonCharacterCardComponent implements OnInit, OnDestroy {
+  characters: Character[] = []; // Arreglo para almacenar los personajes
+  maxCharacterCount: number = 3; // Número máximo de personajes permitidos
+
+  private userSubscription: Subscription | undefined;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.authService.getUserCharacters().subscribe(characters => {
-      this.characters = characters;
-      console.log(characters);
-    });
+    this.userSubscription = this.authService.getUserCharacters()
+      .pipe(take(1)) // Utiliza take(1) para limitar las emisiones a una sola
+      .subscribe(characters => {
+        this.characters = characters;
+        console.log(characters);
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,95 +1,97 @@
-import { Injectable, NgZone } from '@angular/core';
-import { GoogleAuthProvider, user } from '@angular/fire/auth';
+import { Injectable } from '@angular/core';
+import { GoogleAuthProvider } from '@angular/fire/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { User } from '../models/user.model';
 import { Observable, of } from 'rxjs';
-import { switchMap,map } from 'rxjs/operators';
-import { AngularFirestore, AngularFirestoreDocument,AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { switchMap } from 'rxjs/operators';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { Character } from '../models/character.model';
-
-
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthService {
-
+  
   user$: Observable<User | null | undefined>;
-  private usersCollection: AngularFirestoreCollection<User>;
-  users$: Observable<User[]>;
 
   constructor(
     private firebaseAuthenticationService: AngularFireAuth,
     private router: Router,
     private afs: AngularFirestore,
   ) {
-    this.usersCollection = this.afs.collection<User>('users');
-    this.users$ = this.usersCollection.valueChanges();
-
     this.user$ = this.firebaseAuthenticationService.authState.pipe(
       switchMap(user => {
-          // Logged in
         if (user) {
+          console.log('Usuario autenticado:', user);
           return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
         } else {
-          // Logged out
+          console.log('Usuario no autenticado');
           return of(null);
         }
-      })
-    )
+      }),
+      shareReplay(1) // Compartir la misma suscripción entre observadores
+    );
   }
 
-  async googleSignin() {
+  googleSignin() {
     const provider = new GoogleAuthProvider();
-    const credential = await this.firebaseAuthenticationService.signInWithPopup(provider);
-    return this.updateUserData(credential.user);
+    this.firebaseAuthenticationService.signInWithPopup(provider)
+      .then(credential => this.updateUserData(credential.user))
+      .catch(error => {
+        console.error('Error al iniciar sesión con Google:', error);
+      });
   }
 
   private async updateUserData(user) {
     // Sets user data to firestore on login
     const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
   
-    const snapshot = await userRef.get().toPromise();
-    if (snapshot && snapshot.exists) {
-      const data = snapshot?.data();
-
-    const updatedData: Partial<User> = { 
-      uid: user.uid ?? '', 
-      email: user.email, 
-      displayName: user.displayName, 
-      photoURL: user.photoURL,
-      character1: data && data.character1 !== null ? data.character1 : null,
-      character2: data && data.character2 !== null ? data.character2 : null,
-      character3: data && data.character3 !== null ? data.character3 : null,
-      historias: data && data.historias !== null ? data.historias : 0
-    } 
-      
-      return userRef.set(updatedData as User, { merge: true });
-    } else {
-      const initialData: Partial<User> = {
-        uid: user.uid ?? '', 
-        email: user.email, 
-        displayName: user.displayName, 
-        photoURL: user.photoURL,
-        character1: null,
-        character2: null,
-        character3: null,
-        historias: 0
-      }
+    try {
+      const snapshot = await userRef.get().toPromise();
+      if (snapshot && snapshot.exists) {
+        const data = snapshot.data();
   
-      return userRef.set(initialData as User);
+        const updatedData: Partial<User> = {
+          uid: user.uid ?? '',
+          email: user.email,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          character1: data?.character1 || null,
+          character2: data?.character2 || null,
+          character3: data?.character3 || null,
+          historias: data?.historias || 0
+        };
+  
+        await userRef.set(updatedData as User, { merge: true });
+      } else {
+        const initialData: Partial<User> = {
+          uid: user.uid ?? '',
+          email: user.email,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          character1: null,
+          character2: null,
+          character3: null,
+          historias: 0
+        };
+  
+        await userRef.set(initialData as User);
+      }
+    } catch (error) {
+      console.error('Error al actualizar los datos del usuario:', error);
     }
   }
   
-  getUsers(): void {
-    this.users$.subscribe(users => {
-      console.log(users);
+
+  getUsers() {
+    this.afs.collection<User>('users').valueChanges().subscribe(users => {
+      console.log('Usuarios:', users);
     });
   }
 
-  getCurrentUser(): Observable<User | null | undefined>{
+  getCurrentUser(): Observable<User | null | undefined> {
     return this.user$;
   }
 
@@ -97,9 +99,18 @@ export class AuthService {
     return this.user$.pipe(
       switchMap(user => {
         if (user) {
-          const user2: User = { uid: user.uid, email: user.email, displayName: user.email, photoURL: user.photoURL, historias: user.historias };
-          return this.users$.pipe(
-            map(users => users.find(u => u.uid === user2.uid))
+          const user2: User = {
+            uid: user.uid,
+            email: user.email,
+            displayName: user.email,
+            photoURL: user.photoURL,
+            historias: user.historias
+          };
+          return this.afs.collection<User>('users').valueChanges().pipe(
+            switchMap(users => {
+              console.log('Usuarios en búsqueda:', users);
+              return of(users.find(u => u.uid === user2.uid));
+            })
           );
         } else {
           return of(null);
@@ -113,39 +124,25 @@ export class AuthService {
     this.router.navigate(['/']);
   }
 
-
   async saveCharacter(character: Character) {
-    try {
-      // Obtén el usuario autenticado actual
-      const user = await this.firebaseAuthenticationService.currentUser;
-  
-      if (user) {
-        // Define la referencia al documento del usuario
-        const userRef = this.afs.doc(`users/${user.uid}`);
-  
-        // Obtiene el documento actual del usuario
-        const userDoc = await userRef.get().toPromise();
-  
-        // Obtiene los campos de personajes
-        const character1 = userDoc?.get('character1');
-        const character2 = userDoc?.get('character2');
-        const character3 = userDoc?.get('character3');
-  
-        // Realiza la comprobación y guarda el personaje en el primer campo disponible
-        if (!character1) {
-          await userRef.update({ character1: character });
-        } else if (!character2) {
-          await userRef.update({ character2: character });
-        } else if (!character3) {
-          await userRef.update({ character3: character });
-        } else {
-          console.error('Todos los campos de personajes están ocupados. No se puede guardar un nuevo personaje.');
-        }
+    const user = await this.firebaseAuthenticationService.currentUser;
+    if (user) {
+      const userRef = this.afs.doc(`users/${user.uid}`);
+      const userDoc = await userRef.get().toPromise();
+      const character1 = userDoc?.get('character1');
+      const character2 = userDoc?.get('character2');
+      const character3 = userDoc?.get('character3');
+      if (!character1) {
+        await userRef.update({ character1: character });
+      } else if (!character2) {
+        await userRef.update({ character2: character });
+      } else if (!character3) {
+        await userRef.update({ character3: character });
       } else {
-        console.error('Usuario no autenticado. No se puede guardar el personaje.');
+        console.error('Todos los campos de personajes están ocupados. No se puede guardar un nuevo personaje.');
       }
-    } catch (error) {
-      console.error('Error al guardar el personaje:', error);
+    } else {
+      console.error('Usuario no autenticado. No se puede guardar el personaje.');
     }
   }
 
@@ -156,15 +153,12 @@ export class AuthService {
           const characters: Character[] = [];
           if (user.character1 && !characters.includes(user.character1)) {
             characters.push(user.character1);
-            console.log(user.character1);
           }
           if (user.character2 && !characters.includes(user.character2)) {
             characters.push(user.character2);
-            console.log(user.character2);
           }
           if (user.character3 && !characters.includes(user.character3)) {
             characters.push(user.character3);
-            console.log(user.character3);
           }
           return of(characters);
         } else {
@@ -173,6 +167,4 @@ export class AuthService {
       })
     );
   }
-
 }
-
