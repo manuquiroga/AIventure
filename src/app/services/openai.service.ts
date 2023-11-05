16af2745d2ed3837641dcdfa2086a56bc08c5aaa
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-11-05 18:06:07 -0300

    try post to our api

--- src/app/services/openai.service.ts
+++ src/app/services/openai.service.ts
@@ -1,36 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-import { OpenAI } from "openai"
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OpenaiService {
+  private apiUrl = 'http://localhost:8000/post';
 
-  async main()
-  {
-    
-      try {
-         const openai = new OpenAI({
-           apiKey: environment.openai.apiKey,
-           dangerouslyAllowBrowser: true,
-         });
-         const chatCompletion = await openai.chat.completions.create({
-           model: 'gpt-3.5-turbo',
-           messages: [
-             {
-               'role': 'user',
-               'content': 'say this is a test',
-             },
-           ],
-         });
-         console.log(chatCompletion);
-      } catch (err) {
-         console.log(err);
-      }
-     
-    
-  
-}
+  constructor(private http: HttpClient) {}
+
+  postData(question: string) {
+    return this.http.post(this.apiUrl, { prompt: question });
+  }
 }
