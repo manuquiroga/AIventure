aed7dd3573634e61f9dfc6e23ebb26771757cddf
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-11-20 13:14:41 -0300

    more responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   resultado: any;
 
-  constructor(private auth: AuthService){}
+  constructor(private auth: AuthService) {}
   title = 'AIVENTURE';
 
-  
+  isDesktop: boolean = window.innerWidth >= 1000;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isDesktop = window.innerWidth >= 1000;
+  }
 }
